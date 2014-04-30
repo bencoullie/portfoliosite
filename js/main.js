368e8cbd24d2d73f91436462722542310edf8562
Ben <bencoullie@gmail.com>
2014-04-30 06:40:21 -0800

    added fonts, basic boxes and fade ins

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(function(){
+     $('.mainheadone').delay(1000).animate({
+          opacity: 1, top: 30
+     }, 1100);
+     $('.mainheadtwo').delay(2500).animate({
+          opacity: 1
+     }, 700);
+     $('.mainheadthree').delay(4000).animate({
+          opacity: 1
+     }, 500)
+     $('.svgarrow').delay(5000).animate({
+          opacity: 1
+     }, 300);
+});  
 
